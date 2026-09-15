821866c66aebabf2a8718d90392edfb43b8a5f68
dalton <dnickerson@fernleaf.us>
2026-09-15 14:14:48 -0400

    WIP on rest api to add permissions to token.

--- generate_api_token/index.js
+++ generate_api_token/index.js
@@ -1,20 +1,20 @@
-import { readFile } from "fs/promises";
-import { searchItems } from "@esri/arcgis-rest-portal";
-import { slotForKey, updateApiKey, invalidateApiKey } from "@esri/arcgis-rest-developer-credentials";
-import { ArcGISIdentityManager } from "@esri/arcgis-rest-request";
+import {readFile} from "fs/promises";
+import {searchItems} from "@esri/arcgis-rest-portal";
+import {slotForKey, updateApiKey, invalidateApiKey} from "@esri/arcgis-rest-developer-credentials";
+import {ArcGISIdentityManager} from "@esri/arcgis-rest-request";
 
 const default_token = process.env.ARCGIS_TOKEN
 
-async function loadConfig(file_path="../config.json") {
+async function loadConfig(file_path = "../config.json") {
     const config = JSON.parse(await readFile(file_path, "utf8"));
     return config;
 }
 
-async function checkForApp(appTitle, token, portalURL="https://www.arcgis.com") {
+async function checkForApp(appTitle, token, portalURL = "https://www.arcgis.com") {
     const authentication = await ArcGISIdentityManager.fromToken({token: token});
     let searchQuery = `title:"${appTitle}" AND type:"Application"`
-    const response = await searchItems({ q: searchQuery, authentication });
-    if (response.total > 0){
+    const response = await searchItems({q: searchQuery, authentication});
+    if (response.total > 0) {
         const existingApp = response.results[0];
         console.log(`Application found with id: ${existingApp.id}, title: ${existingApp.title}`)
         return existingApp;
@@ -23,6 +23,26 @@ async function checkForApp(appTitle, token, portalURL="https://www.arcgis.com") 
     return null;
 }
 
+async function updateAppFeatureServices(appItemID, featureServices, token) {
+    const authentication = await ArcGISIdentityManager.fromToken({token: token});
+
+    try {
+        const updateResponse = await updateApiKey({
+            itemId: appItemID,
+            privilege: featureServices.map((service) => `portal:app:access:item:${service}`),
+            authentication
+        });
+        console.log(`Updated app with id: ${appItemID}, featureServices: ${featureServices}`)
+    } catch (error) {
+        console.log(`Error updating app with id: ${appItemID}, featureServices: ${featureServices}`)
+        console.log(error)
+    }
+}
 
 let config = await loadConfig();
-await checkForApp(config.token_name, default_token);
+const existingApp = await checkForApp(config.token_name, default_token);
+if (existingApp) {
+    await updateAppFeatureServices(existingApp.id, config.feature_services, default_token);
+} else {
+    console.log(`No application found with title: ${config.token_name}`)
+}
